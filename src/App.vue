34aa72391b255bae7cddf2055c845f4a2518f7e2
往逸 <1159294999@qq.com>
2019-05-12 21:09:37 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="stack-wrapper">
+      <stack ref="stack" :pages="someList" :stackinit="stackinit"></stack>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  import stack from './components/Stack'
+  export default {
+    data () {
+      return {
+        someList: [],
+        stackinit: {
+          visible: 3
+        }
+      }
+    },
+    mounted () {
+      let that = this
+      setTimeout(function () {
+        that.someList = [
+          {
+            html: '<img src="https://img.xiangshengclub.com/MTU1NjM3MjY0NTYxNCMxMDAjanBn.jpg" alt="01">'
+          },
+          {
+            html: '<img src="https://img.xiangshengclub.com/MTU1NjM3MjY0NTYxNCMxMDAjanBn.jpg" alt="02">'
+          },
+          {
+            html: '<img src="https://img.xiangshengclub.com/MTU1NjM3MjY0NTYxNCMxMDAjanBn.jpg" alt="03">'
+          },
+          {
+            html: '<img src="https://img.xiangshengclub.com/MTU1NjM3MjY0NTYxNCMxMDAjanBn.jpg" alt="04">'
+          },
+          {
+            html: '<img src="https://img.xiangshengclub.com/MTU1NjM3MjY0NTYxNCMxMDAjanBn.jpg" alt="05">'
+          },
+          {
+            html: '<img src="https://img.xiangshengclub.com/MTU1NjM3MjY0NTYxNCMxMDAjanBn.jpg" alt="06">'
+          },
+          {
+            html: '<img src="https://img.xiangshengclub.com/MTU1NjM3MjY0NTYxNCMxMDAjanBn.jpg" alt="07">'
+          }
+        ]
+      }, 2000)
+    },
+    components: {
+      stack
+    },
+    methods: {
+      prev () {
+        this.$refs.stack.$emit('prev')
+      },
+      next () {
+        this.$refs.stack.$emit('next')
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+    padding: 0;
+    min-width: 100vw;
+    min-height: 100vh;
+    background: url('./assets/bg.jpg') no-repeat;
+    background-size: cover;
+  }
+
+  .stack-wrapper {
+    margin: 0 auto;
+    padding-top: 10vh;
+    position: relative;
+    z-index: 1000;
+    width: 80vw;
+    height: 50vh;
+    list-style: none;
+    pointer-events: none;
+  }
 </style>
